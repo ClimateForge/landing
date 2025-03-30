aeb7392794d5fb0e1c5b01fe9ccc39eb833a0a34
Anselm <anselmjs@icloud.com>
2025-03-30 18:29:52 -0500

    remote patts

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,18 @@ const nextConfig = {
 		path: "/landing/_next/image", // Ensure this matches your basePath
 		unoptimized: false, // REQUIRED (default)
 		domains: ["climateforge.ai", "climateforge-landing.vercel.app"],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "climateforge-landing.vercel.app",
+				pathname: "/landing/**",
+			},
+			{
+				protocol: "https",
+				hostname: "www.climateforge.ai",
+				pathname: "/landing/**",
+			},
+		],
 	},
 };
 
